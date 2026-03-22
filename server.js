08fa4e135cb7b773d342a7441fd1a7799215e296
mendirizel770 <mendirizel770@gmail.com>
2026-03-23 01:23:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
   res.send("🚀 Million Pixel Server עובד!");
 });
 
-const PORT = 4242;
+const PORT = process.env.PORT || 4242;
 app.listen(PORT, () => {
   console.log("Server running on " + PORT);
 });
