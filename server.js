edd3b34a1578457339bad70a0bb43b702fa1d0ed
mendirizel770 <mendirizel770@gmail.com>
2026-03-23 01:13:22 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+let pixels = [];
+
+app.get("/pixels", (req, res) => {
+  res.json(pixels);
+});
+
+app.post("/buy", (req, res) => {
+  const { index, text, image, link } = req.body;
+
+  pixels.push({
+    index,
+    text,
+    image,
+    link,
+    purchased: true,
+  });
+
+  res.json({ success: true });
+});
+
+app.get("/", (req, res) => {
+  res.send("🚀 Million Pixel Server עובד!");
+});
+
+const PORT = 4242;
+app.listen(PORT, () => {
+  console.log("Server running on " + PORT);
+});
